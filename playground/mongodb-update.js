6de5e6c98f2f45e44329ffa4adee66680ab31faa
Hein Dauven <heindauven@gmail.com>
2018-04-30 15:49:26 +0200

    Experimenting with MongoDB-node file updating

---
+++ playground/mongodb-update.js
@@ -0,0 +1,41 @@
+//const MongoClient = require('mongodb').MongoClient;
+const { MongoClient, ObjectID } = require('mongodb');
+
+var obj = new ObjectID();
+console.log(obj);
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if (err) {
+    return console.log('Unable to connect to MongoDB server', err);
+  }
+  console.log('Connected to MongoDB server');
+
+  db.collection('Todos').findOneAndUpdate({
+    _id: new ObjectID('5ae71998289d909573af1ac0')
+  }, {
+    $set: {
+      completed: true
+    }
+  }, {
+    returnOriginal: false
+  }).then((result) => {
+    console.log(result);
+  });
+
+  db.collection('Users').findOneAndUpdate({
+    _id: new ObjectID('5ae1eec4239c6024ae757c3b')
+  }, {
+    $set: {
+      name: 'hien'
+    },
+    $inc: {
+      age: 1
+    }
+  }, {
+    returnOriginal: false
+  }).then((result) => {
+    console.log(result);
+  });
+
+  //db.close();
+});
