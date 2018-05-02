e6b449aac6a11ac0f9c9ce086801ec08328e3cc6
Hein Dauven <heindauven@gmail.com>
2018-05-03 01:18:09 +0200

    Add tests for GET /users/me and POST /users

--- server/tests/seed/seed.js
+++ server/tests/seed/seed.js
@@ -1,6 +1,33 @@
 const { ObjectID } = require('mongodb');
+const jwt = require('jsonwebtoken');
 
 const { Todo } = require('./../../models/todo');
+const { User } = require('./../../models/user');
+
+const userOneId = new ObjectID();
+const userTwoId = new ObjectID();
+const users = [{
+  _id: userOneId,
+  email: 'andrew@example.com',
+  password: 'userOnePass',
+  tokens: [{
+    access: 'auth',
+    token: jwt.sign({_id: userOneId, access: 'auth'}, 'abc123').toString()
+  }]
+}, {
+  _id: userTwoId,
+  email: 'jen@example.com',
+  password: 'userTwoPass'
+}];
+
+const populateUsers = (done) => {
+  User.remove({}).then(() => {
+    var userOne = new User(users[0]).save();
+    var userTwo = new User(users[1]).save();
+
+    return Promise.all([userOne, userTwo])
+  }).then(() => done());
+};
 
 const todos = [{
   _id: new ObjectID(),
@@ -18,4 +45,4 @@ const populateTodos = (done) => {
   }).then(() => done());
 };
 
-module.exports = { todos, populateTodos };
+module.exports = { users, populateUsers, todos, populateTodos };
